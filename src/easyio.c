147c3e5a1f0cfcaecc949652db1c43172dc7a26a
Ryan McCullagh <me@ryanmccullagh.com>
2015-08-14 15:42:08 -0700

    initial commit

---
+++ src/easyio.c
@@ -0,0 +1,103 @@
+/*
+ *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <easyio.h>
+
+#define REQUIRED(p) do { \
+	if(!p) { \
+		return NULL; \
+	} \
+} while(0)
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void debug_log(const char* s)
+{
+	#if LIBEASY_IO_DEBUG
+		fprintf(stderr, "%s:%d:%s: %s\n",
+			__FILE__, __LINE__, __func__, s);
+	#endif
+}
+
+/**
+ * file_get_contents:
+ * @path: the path to use to open the file
+ *
+ * Attempts to read the full contents of file located
+ * at @path into a null terminated buffer. The file is only
+ * read if it is a regular file.
+ *
+ * Returns: a pointer to the start of the buffer, or NULL
+ */
+LIBEASY_IO_API char* file_get_contents(const char* path)
+{
+	REQUIRED(path);
+	
+	struct stat buffer;
+	int 	    status;
+	void*	    content;
+	char*	    ret;
+	FILE*	    fp;
+	size_t      bytes_read;
+
+	status = stat(path, &buffer);
+
+	if(status == -1) {
+		debug_log("cannot stat file");
+		return NULL;
+	}
+
+	if(!S_ISREG(buffer.st_mode)) {
+		debug_log("not a regular file");
+		return NULL;
+	}
+
+	content = malloc(buffer.st_size + 1);
+	
+	if(!content) {
+		debug_log("malloc failed");
+		return NULL;
+	}
+
+	fp = fopen(path, "r");
+
+	if(!fp) {
+		debug_log("cannot open file");
+		return NULL;
+	}
+	
+	bytes_read = fread(content, buffer.st_size, 1, fp);
+
+	fclose(fp);
+
+	ret = (char *)content;
+
+	ret[buffer.st_size] = '\0';
+
+	return ret;
+}	
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/easyio.h
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBEASY_IO_H
+#define __LIBEASY_IO_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_LIBEASY_IO_DEBUG
+  #define LIBEASY_IO_DEBUG 1
+#else
+  #define LIBEASY_IO_DEBUG 0
+#endif
+
+#if defined _WIN32 || defined __CYGWIN__
+  #define LIBEASY_IO_HELPER_DLL_IMPORT __declspec(dllimport)
+  #define LIBEASY_IO_HELPER_DLL_EXPORT __declspec(dllexport)
+  #define LIBEASY_IO_HELPER_DLL_LOCAL
+#else
+  #if __GNUC__ >= 4
+    #define LIBEASY_IO_HELPER_DLL_IMPORT __attribute__ ((visibility ("default")))
+    #define LIBEASY_IO_HELPER_DLL_EXPORT __attribute__ ((visibility ("default")))
+    #define LIBEASY_IO_HELPER_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+  #else
+    #define LIBEASY_IO_HELPER_DLL_IMPORT
+    #define LIBEASY_IO_HELPER_DLL_EXPORT
+    #define LIBEASY_IO_HELPER_DLL_LOCAL
+  #endif
+#endif
+
+#ifdef BUILDING_LIBEASY_IO
+  #define LIBEASY_IO_API LIBEASY_IO_HELPER_DLL_EXPORT
+#else
+  #define LIBEASY_IO_API LIBEASY_IO_HELPER_DLL_IMPORT
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern LIBEASY_IO_API char* file_get_contents(const char*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LIBEASY_IO_H */
